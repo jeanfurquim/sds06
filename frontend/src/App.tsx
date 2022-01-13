6f6036d99481e4549525ab5784ee61ca66dd36df
jeanfurquim <jean.furquim@jpisistemas.com.br>
2022-01-12 22:47:01 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import Navbar from "components/Navbar";
+import React from "react";
 
 function App() {
   return (
-  <h1>Página Principal</h1>
+    <Navbar />
   );
 }
 
